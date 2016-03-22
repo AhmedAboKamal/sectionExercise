719cfb7459a39d041e6cf5d3566a3ac39a2d56c1
User <User@172.31.45.30>
2016-03-22 10:55:16 +0200

    new print command add

--- src/jghxcv/piosjf.java
+++ src/jghxcv/piosjf.java
@@ -4,6 +4,9 @@ public class piosjf {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");//Display Hello
+		System.out.println("Hello");//Display Hello one more time
+		
+		
 
 	}
 
